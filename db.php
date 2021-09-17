e0f6e5360c462415d0f4786039e3f4b210525dbe
M. Six Silberman <six@wtf.tw>
2021-09-17 20:44:16 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,3 +1,3 @@
 <?php
-$db = new PDO('mysql:host=localhost;dbname=wagepledgedb;charset=utf8', 'ENTER_DB_USER_HERE', 'ENTER_DB_PASSWORD_HERE');
+$db = new PDO('mysql:host=localhost;dbname=ENTER_DB_NAME_HERE;charset=utf8', 'ENTER_DB_USER_HERE', 'ENTER_DB_PASSWORD_HERE');
 
